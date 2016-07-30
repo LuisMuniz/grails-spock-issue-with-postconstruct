caa586cb5a591a554980bce004b824359bbeedb5
LuisMuniz <luis.muniz@b2boost.com>
2016-07-30 18:27:36 +0200

    Cleanup

--- test/unit/issue/MyServiceSpec.groovy
+++ test/unit/issue/MyServiceSpec.groovy
@@ -1,20 +1,17 @@
-package issue
+    package issue
 
-import grails.test.mixin.TestFor
-import spock.lang.Specification
+    import grails.test.mixin.TestFor
+    import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
-@TestFor(MyService)
-class MyServiceSpec extends Specification {
+    @TestFor(MyService)
+    class MyServiceSpec extends Specification {
 
-    def setup() {
-        grailsApplication.config.myProperty = 'myValue'
+        def setup() {
+            grailsApplication.config.myProperty = 'myValue'
+        }
+
+        void "It fails to initialize the service"() {
+            expect:
+            false // this is never executed
+        }
     }
-
-    void "It fails to initialize the service"() {
-        expect:
-        false // this is never executed
-    }
-}
