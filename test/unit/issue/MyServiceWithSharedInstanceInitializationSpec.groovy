bd81c183c26b7c15ad95a29fe916ffc8d2dea03a
LuisMuniz <luis.muniz@b2boost.com>
2016-07-30 18:27:54 +0200

    Cleanup

--- test/unit/issue/MyServiceWithSharedInstanceInitializationSpec.groovy
+++ test/unit/issue/MyServiceWithSharedInstanceInitializationSpec.groovy
@@ -1,27 +1,24 @@
-package issue
+    package issue
 
-import grails.test.mixin.TestFor
-import spock.lang.Specification
+    import grails.test.mixin.TestFor
+    import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
-@TestFor(MyService)
-class MyServiceWithSharedInstanceInitializationSpec extends Specification {
+    @TestFor(MyService)
+    class MyServiceWithSharedInstanceInitializationSpec extends Specification {
 
-    //Initializing the shared instance grailsApplication lets the @PostConstruct work, but will fail during method test
-    //because the instance that was initialized is the shared instance
-    def setupSpec() {
-        grailsApplication.config.myProperty = 'myValue'
+        //Initializing the shared instance grailsApplication lets the @PostConstruct work, but will fail during method test
+        //because the instance that was initialized is the shared instance
+        def setupSpec() {
+            grailsApplication.config.myProperty = 'myValue'
+        }
+
+        void "It fails to execute doSomething()"() {
+            when:
+            service.doSomething()
+
+            then:
+            def e = thrown(NullPointerException)
+            e.message == 'Cannot invoke method toLowerCase() on null object'
+            service.property == null
+        }
     }
-
-    void "It fails to execute myProperty"() {
-        when:
-        service.doSomething()
-
-        then:
-        def e = thrown(NullPointerException)
-        e.message == 'Cannot invoke method toLowerCase() on null object'
-        service.property == null
-    }
-}
